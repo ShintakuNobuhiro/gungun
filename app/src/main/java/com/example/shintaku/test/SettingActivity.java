644239c200e100cea0c6bf9d1b7aa984d0022580
Shintaku <ukyankyan11@gmail.com>
2015-09-20 20:59:45 +0900

    Commit(Error)

--- app/src/main/java/com/example/shintaku/test/SettingActivity.java
+++ app/src/main/java/com/example/shintaku/test/SettingActivity.java
@@ -3,27 +3,69 @@ package com.example.shintaku.test;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class SettingActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+public class SettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        Log.d("a", "a");
+        final String[] description = new String[2];
+
+        ASyncGet asyncGet = new ASyncGet(new AsyncCallback() {
+            public void onPreExecute() {
+                // do something
+            }
+            public void onProgressUpdate(int progress) {
+                // do something
+            }
+            public void onPostExecute(final String result) {
+                Log.d("start","start");
+                if (result != null) {
+                    try {
+                        JSONObject json = new JSONObject(result);
+                        JSONArray missions = json.getJSONArray("missions");
+
+                        for (int i = 0; i < missions.length(); i++) {
+                            JSONObject mission = missions.getJSONObject(i);
+                            description[i] = mission.getString("description");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Log.e("result","null");
+                }
+            }
+
+            public void onCancelled() {
+                // do something
+            }
+        });
+        asyncGet.execute("https://railstutorial-ukyankyan-1.c9.io/users/1.json");
+
         Button btnNext = (Button) this.findViewById(R.id.toggleButton); //健康
+        final Button finalBtnNext = btnNext;
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String txt = "健康";
                 Settings text = new Settings(txt);
+                finalBtnNext.setText(description[0]);
 
                 Intent intent = new Intent(SettingActivity.this, Setting2Activity.class);
                 intent.putExtra("test_result", text);
                 startActivity(intent);
+
             }
         });
         btnNext = (Button) this.findViewById(R.id.toggleButton3); //運動とお仕事
